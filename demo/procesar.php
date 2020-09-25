e8b323093851842c7dec7242d8908cd60146102b
Ginneidy <64049555+Ginneidy@users.noreply.github.com>
2020-09-25 10:07:30 -0500

    prueba 3
    :(

--- demo/procesar.php
+++ demo/procesar.php
@@ -1,9 +1,7 @@
 <?php
 if (!isset($_POST['submit'])) {
     //This page should not be accessed directly. Need to submit the form.
-    echo "error; you need to submit the form!";
-    exit;
-
+    
     $nombre = $_GET['fname'];
     $apellidos = $_GET['lname'];
     $email = $_GET['email'];
