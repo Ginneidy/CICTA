0907488829b78b3792c30c8709f6432cb6171f19
Ginneidy <64049555+Ginneidy@users.noreply.github.com>
2020-09-25 10:39:31 -0500

    form

--- demo/procesar.php
+++ demo/procesar.php
@@ -46,25 +46,4 @@ if (isset($_POST['submit'])) {
     //done. redirect to thank-you page.
     header('Location: index.html');
 
-
-    // Function to validate against any email injection attempts
-    function IsInjected($str)
-    {
-        $injections = array(
-            '(\n+)',
-            '(\r+)',
-            '(\t+)',
-            '(%0A+)',
-            '(%0D+)',
-            '(%08+)',
-            '(%09+)'
-        );
-        $inject = join('|', $injections);
-        $inject = "/$inject/i";
-        if (preg_match($inject, $str)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 }
