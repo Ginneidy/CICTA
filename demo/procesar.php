ab6d43d199242cbe86afe5a22008717a3f4485fc
Ginneidy <64049555+Ginneidy@users.noreply.github.com>
2020-09-25 10:18:39 -0500

    Prubea 4
    creo

--- demo/procesar.php
+++ demo/procesar.php
@@ -1,5 +1,9 @@
 <?php
-if (!isset($_POST['submit'])) {
+if(!isset($_POST['submit'])){
+    echo "<p>Debes llenar el formulario</p>";
+    exit;
+}
+if (isset($_POST['submit'])) {
     //This page should not be accessed directly. Need to submit the form.
     
     $nombre = $_GET['fname'];
@@ -13,17 +17,17 @@ if (!isset($_POST['submit'])) {
     //Validate first
     if (empty($nombre) || empty($email) || empty($apellidos) || empty($identificacion) || empty($profesion) || empty($empresa) || empty($pais)) {
         echo "Todos los datos son obligatorios!";
-        if(!is_numeric($identificacion)){
-            echo "<p> Número de identificación invalido </p>";
-        }
+        
         exit;
     }
-
     if (IsInjected($email)) {
         echo "Email incorrecto!";
         exit;
     }
-
+    if(!is_numeric($identificacion)){
+        echo "<p> Número de identificación invalido </p>";
+        exit;
+    }
     $email_from = 'ginneidy@gmial.com'; //<== update the email address
     $email_subject = "Nuevo formulario";
     $email_body = "Un nuevo usuario se ha registrado: \n" .
@@ -32,12 +36,12 @@ if (!isset($_POST['submit'])) {
         "Email: $email.\n" .
         "Identificación: $identificacion.\n" .
         "profesion: $profesion.\n";
-    "emrpesa: $empresa.\n";
-    "pais: $pais.\n";
+        "emrpesa: $empresa.\n";
+        "pais: $pais.\n";
 
     $to = "ginneidy@gmail.com"; //<== update the email address
     $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $visitor_email \r\n";
+    $headers.= "Reply-To: $email \r\n";
     //Send the email!
     mail($to, $email_subject, $email_body, $headers);
     //done. redirect to thank-you page.
